1400fe205665a1c142c49f8313c236dc49c2df1a
ijh950809-dotcom <ijh950809@gmail.com>
2026-02-04 11:55:49 +0900

    Add files via upload

--- app/header/page.js
+++ app/header/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
@@ -7,6 +8,9 @@ function Header() {
 
   //1.상태변수 선언
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [toggleMenuOpen, setToggleMenuOpen] = useState(false);
 
   //2.페이지 로딩시 토큰값을 한번만 불러온다.
   useEffect(() => {
@@ -19,21 +23,56 @@ function Header() {
   }, []);
   return (
     <header>
-      <h1>상단로고</h1>
+      <div class="logo_icon">
+        <i class="fas fa-user"></i>
+        <h1>
+          {/* next.js에서 이미지 넣기 방법 1.(권장/상단에 선언)*/}
 
-      <nav>
-        {isLoggedIn ? (
-          <>
-            <Link href='../logout'>로그아웃</Link></>
-        ) : (
-          <>
-            <Link href='../login'>로그인</Link>&#10072; &nbsp;
-            <Link href='../register'>회원가입</Link>
-          </>
-        )
-        }
-      </nav>
-    </header>
+          <Image
+            src="/images/logo.png"
+            alt="상단로고"
+            width={237}
+            height={50}
+          />
+          {/* next.js에서 이미지 넣기 방법 2. */}
+          {/* <img src="/images/logo.png" alt="상단로고" /> */}
+
+        </h1>
+        <i class="fas fa-bars" onClick={() => {
+          setMenuOpen(true)
+        }}></i>
+      </div>
+
+
+      <div class="toggle_nav" style={{ right: menuOpen ? '0%' : '-100%' }}>
+        <i class="fas fa-times" onClick={() => setMenuOpen(false)}></i>
+
+        <ul class="toggle_menu">
+          <li onClick={() => setToggleMenuOpen(true)}>ESPRESSO BAR
+            <ul style={{ display: toggleMenuOpen ? 'block' : 'none' }}>
+              <li>ESPRESSO BAR</li>
+              <li>CENTRO</li>
+            </ul>
+          </li>
+          <li>PASCUCCI</li>
+          <li>MENU</li>
+          <li>NEWS</li>
+          <li>CUSTOMER</li>
+        </ul>
+        <nav>
+          {isLoggedIn ? (
+            <>
+              <Link href='../logout'>로그아웃</Link></>
+          ) : (
+            <>
+              <Link href='../login'>로그인</Link>&#10072; &nbsp;
+              <Link href='../register'>회원가입</Link>
+            </>
+          )
+          }
+        </nav>
+      </div>
+    </header >
   );
 }
 
